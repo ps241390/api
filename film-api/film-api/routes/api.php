ae675840f171269a30e8b4c87398262f955199ba
Dennis Visser <ps241390@edu.summacollege.nl>
2025-01-03 15:38:25 +0100

    sd

--- film-api/film-api/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FilmController;
-
-Route::get('/films', [FilmController::class, 'index']);
-Route::post('/films', [FilmController::class, 'store']);
-Route::get('/films/{film}', [FilmController::class, 'show']);
